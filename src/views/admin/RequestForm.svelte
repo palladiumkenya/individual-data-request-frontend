34afa22e9b2936724722f056119b15e28e87d318
NewtonMutugi <newtonmut197@gmail.com>
2024-08-21 16:28:11 +0300

    Created Request Form

--- src/views/admin/RequestForm.svelte
+++ src/views/admin/RequestForm.svelte
@@ -1,15 +1,12 @@
 <script>
   // core components
-  import CardSettings from 'components/Cards/CardSettings.svelte';
+  import CardRequestForm from 'components/Cards/CardRequestForm.svelte';
   import CardProfile from 'components/Cards/CardProfile.svelte';
   export let location;
 </script>
 
 <div class="flex flex-wrap">
-  <div class="w-full lg:w-8/12 px-4">
-    <CardSettings />
-  </div>
-  <div class="w-full lg:w-4/12 px-4">
-    <CardProfile />
+  <div class="w-full lg:w-full px-4">
+    <CardRequestForm />
   </div>
 </div>
