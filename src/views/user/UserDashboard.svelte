abe30ca9a5b82bc06036de936d32a4ecfdf2075b
NewtonMutugi <newtonmut197@gmail.com>
2024-08-28 13:39:12 +0300

    Changed User Dashboard to display dynamic table

--- src/views/user/UserDashboard.svelte
+++ src/views/user/UserDashboard.svelte
@@ -1,13 +1,13 @@
+<!-- UserDashboard.svelte -->
 <script>
-  // core components
-  import CardCompleteApprovals from '../../components/Cards/CardCompleteApprovals.svelte';
+  import CardApprovalsTable from '../../components/Cards/CardApprovalsTable.svelte';
   export let location;
 </script>
 
 <div>
   <div class="flex flex-wrap mt-4">
     <div class="w-full xl:w-full mb-12 xl:mb-0 px-4">
-      <CardCompleteApprovals />
+      <CardApprovalsTable />
     </div>
   </div>
 </div>
