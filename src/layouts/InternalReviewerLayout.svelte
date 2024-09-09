3a6ac4a44839f7db4a003459c299c7fc5e8d9d6d
NewtonMutugi <newtonmut197@gmail.com>
2024-09-09 09:09:42 +0300

    Renamed dashboard urls

--- src/layouts/InternalReviewerLayout.svelte
+++ src/layouts/InternalReviewerLayout.svelte
@@ -1,20 +1,20 @@
 <script>
     import { Router, Route } from 'svelte-routing';
-  
+
     // components for this layout
     import InternalReviewerNavbar from '../components/Navbars/InternalReviewerNavbar.svelte';
     import InternalReviewerHeaderStats from '../components/Headers/InternalReviewerHeaderStats.svelte';
-  
-  
-  
+
+
+
     // pages for this layout
     import InternalReviewerSidebar from '../components/Sidebar/InternalReviewerSidebar.svelte';
     import InternalReviewerDashboard from '../views/internalreviewer/internalReviewerDashboard.svelte';
-  
+
     export let location;
     //export let admin = '';
   </script>
-  
+
   <div>
     <InternalReviewerSidebar {location} />
     <div class="relative md:ml-64 bg-blueGray-100">
@@ -22,10 +22,9 @@
       <InternalReviewerHeaderStats />
       <div class="mx-auto w-full -m-24">
         <Router url="internalreviewer">
-          <Route path="internalreviewerdashboard" component={InternalReviewerDashboard} />
+          <Route path="dashboard" component={InternalReviewerDashboard} />
         </Router>
         <!--      <Footer />-->
       </div>
     </div>
   </div>
-  
--- src/layouts/assigneeLayout.svelte
+++ src/layouts/assigneeLayout.svelte
@@ -1,33 +1,30 @@
 <script>
-    import { Router, Route } from 'svelte-routing';
-  
-    // components for this layout
-    import AssigneeNavbar from '../components/Navbars/assigneeNavbar.svelte';
-    import AssigneeHeaderStats from '../components/Headers/assigneeHeaderStats.svelte';
-  
-  
-  
-    // pages for this layout
-    import AssigneeDashboard from '../views/assignee/assigneeDashboard.svelte';
-    import AssigneeSidebar from '../components/Sidebar/assigneeSidebar.svelte';
-    import AssigneeRequestForm from '../views/assignee/assigneeRequestForm.svelte';
-  
-    export let location;
-    //export let admin = '';
-  </script>
-  
-  <div>
-    <AssigneeSidebar {location} />
-    <div class="relative md:ml-64 bg-blueGray-100">
+  import { Router, Route } from 'svelte-routing';
+
+  // components for this layout
+  import AssigneeNavbar from '../components/Navbars/assigneeNavbar.svelte';
+  import AssigneeHeaderStats from '../components/Headers/assigneeHeaderStats.svelte';
+
+  // pages for this layout
+  import AssigneeDashboard from '../views/assignee/assigneeDashboard.svelte';
+  import AssigneeSidebar from '../components/Sidebar/assigneeSidebar.svelte';
+  import AssigneeRequestForm from '../views/assignee/assigneeRequestForm.svelte';
+
+  export let location;
+  //export let admin = '';
+</script>
+
+<div>
+  <AssigneeSidebar {location} />
+  <div class="relative md:ml-64 bg-blueGray-100">
     <AssigneeNavbar />
-      <AssigneeHeaderStats />
-      <div class="mx-auto w-full -m-24">
-        <Router url="user">
-          <Route path="assigneedashboard" component={AssigneeDashboard} />
-          <Route path="new-request" component={AssigneeRequestForm} />
-        </Router>
-        <!--      <Footer />-->
-      </div>
+    <AssigneeHeaderStats />
+    <div class="mx-auto w-full -m-24">
+      <Router url="user">
+        <Route path="dashboard" component={AssigneeDashboard} />
+        <Route path="new-request" component={AssigneeRequestForm} />
+      </Router>
+      <!--      <Footer />-->
     </div>
   </div>
-  
+</div>
